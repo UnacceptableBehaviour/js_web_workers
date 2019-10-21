322e1eeb22a67cb33b295910c8d4cf4372616beb
Simon <simon@ferdiesfoodlab.co.uk>
2019-10-21 15:51:49 +0100

    serve w/ node

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # js_web_workers
-Experiments w web workers
+Experiments with Web workers & Service Workers - Working offline
 
 
-
-### Web workers  
+## Web workers  
 Initial vid: https://www.youtube.com/watch?v=EiPytIxrZtU
 
 No access to DOM, window, parent? object (nothing that interacts with the web page)
@@ -21,14 +20,14 @@ Fine in Firefox!
 ### Debugging service workers:
 #### in Chrome
 https://www.youtube.com/watch?v=tuRPSaSiK_c
-#### in Friefox
+#### in Firefox
 
 
 
-### Web worker vs Service Workers vs Worklets
+## Web worker vs Service Workers vs Worklets
 https://bitsofco.de/web-workers-vs-service-workers-vs-worklets/
 
-** have very little overlap in what they are used for!! **
+** They have very little overlap in what they are used for!! **
 
 Web Worker - Generic extra thread, purpose off load heavy work, free up main thread.
 
@@ -44,13 +43,13 @@ Worklet - Worklets are a very lightweight, highly specific, worker.
 ## SERVICE WORKERS
 https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
-Gothcas
+Gotchas
 https://gist.github.com/Rich-Harris/fd6c3c73e6e707e312d7c5d7d0f3b2f9
 
 More important fundamental gotchas - only in https or local NOT file or http
 https://stackoverflow.com/questions/39136625/service-worker-registration-failed
-serve w/ node
-http-server -p 8000 --cors
+serve w/ node (local)
+http-server -p 8000					#
 http://127.0.0.1:8000
 
 
--- web-worker.js
+++ web-worker.js
@@ -6,51 +6,51 @@ self.addEventListener('message', (ev)=>{
     console.log('\n\nWeb worker started with data: ', ev.data);
     console.log('Inside WW event handler.', ev);
     
-    //let data = ev.data;
-    //
-    ////if ('cmd' in data) { // gives TypeError: invalid 'in' operand data if string
-    //if (data.hasOwnProperty('cmd') in data) {
-    //    switch(data['cmd']){
-    //        case 'f2':
-    //            self.postMessage('Ping reply...');
-    //            break;
-    //
-    //        //case 'fetch':
-    //        //    let url = 'http://jsonplaceholder.typicode.com/posts';
-    //        //    console.log('about to do the fetch for the data');
-    //        //    
-    //        //    fetch(url)
-    //        //    .then(response=>response.json() )
-    //        //    .then( data =>{
-    //        //        self.postMessage( data );
-    //        //    })
-    //        //    .catch(err=>console.log(err));
-    //        //    
-    //        //    break;
-    //        default:
-    //            console.log(`Unexpected message: [${data['cmd']}]`);
-    //            console.log(`type: [${typeof(data['cmd'])}]`);
-    //            // self.postMessage('Closing web worker');
-    //            // self.close();
-    //    }        
-    //} else {
-    //    switch(data){
-    //        case 'Operations HOT':
-    //            // this message is picked up by workerMessaged - using 
-    //            // worker.addEventListener('message', workerMessaged);  <  <  <  <
-    //            // in main javascript                                              |
-    //            self.postMessage('Web Worker Started'); // post message to self - triggers
-    //            break;
-    //        case 'Ping':
-    //            self.postMessage('Ping reply...');
-    //            break;
-    //        default:
-    //            console.log(`Unexpected message: [${data}]`);
-    //            console.log(`type: [${typeof(data)}]`);
-    //            // self.postMessage('Closing web worker');
-    //            // self.close();
-    //    }
-    //}
+    let data = ev.data;
+    
+    //if ('cmd' in data) { // gives TypeError: invalid 'in' operand data if string
+    if (data.hasOwnProperty('cmd')) {
+        switch(data['cmd']){
+            case 'f2':
+                self.postMessage('Ping reply...');
+                break;
+    
+            //case 'fetch':
+            //    let url = 'http://jsonplaceholder.typicode.com/posts';
+            //    console.log('about to do the fetch for the data');
+            //    
+            //    fetch(url)
+            //    .then(response=>response.json() )
+            //    .then( data =>{
+            //        self.postMessage( data );
+            //    })
+            //    .catch(err=>console.log(err));
+            //    
+            //    break;
+            default:
+                console.log(`Unexpected message: [${data['cmd']}]`);
+                console.log(`type: [${typeof(data['cmd'])}]`);
+                // self.postMessage('Closing web worker');
+                // self.close();
+        }        
+    } else {
+        switch(data){
+            case 'Operations HOT':
+                // this message is picked up by workerMessaged - using 
+                // worker.addEventListener('message', workerMessaged);  <  <  <  <
+                // in main javascript                                              |
+                self.postMessage('Web Worker Started'); // post message to self - triggers
+                break;
+            case 'Ping':
+                self.postMessage('Ping reply...');
+                break;
+            default:
+                console.log(`Unexpected message: [${data}]`);
+                console.log(`type: [${typeof(data)}]`);
+                // self.postMessage('Closing web worker');
+                // self.close();
+        }
+    }
     
 
     
